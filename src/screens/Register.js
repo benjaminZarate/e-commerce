89a73fc3cdcabb2e92a324f5dc019c83666831d0
Benjamin Zarate <benja_zarate@hotmail.es>
2023-11-10 08:48:11 -0300

    Add register screen

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -1,30 +1,97 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useState } from 'react'
-import { createUserWithEmailAndPassword } from '@firebase/auth';
-import { firebase_auth } from '../firebase/firebaseAuth';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Pressable,
+} from "react-native";
+import { colors } from "../theme/colors";
+import { firebase_auth } from "../firebase/firebaseAuth";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
-const Register = ({navigation}) => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+const Register = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleRegister = async () => {
-        try {
-            const response = await createUserWithEmailAndPassword(firebase_auth, email, password);
-            navigation.navigate("Login");
-        } catch (e) {
-            console.log(e);
-        }
+  const handleRegister = async () => {
+    try {
+      const response = await createUserWithEmailAndPassword(
+        firebase_auth,
+        email,
+        password
+      );
+      navigation.navigate("login");
+    } catch (e) {
+      console.log("Error en registro", e);
     }
+  };
 
   return (
-    <View>
-      <Text>Register</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Registro</Text>
+      <TextInput
+        placeholder="Escriba su email aquí.."
+        style={styles.input}
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+      />
+      <TextInput
+        placeholder="Contraseña"
+        secureTextEntry
+        style={styles.input}
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+      />
+      <TouchableOpacity style={styles.button} onPress={() => handleRegister()}>
+        <Text style={styles.buttonText}>Registrarse</Text>
+      </TouchableOpacity>
+      <Pressable onPress={() => navigation.navigate("login")}>
+        <Text style={styles.registroText}>
+          Ya tienes cuenta? Iniciar Sesión
+        </Text>
+      </Pressable>
     </View>
-  )
-}
+  );
+};
 
-export default Register
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  input: {
+    width: "85%",
+    height: 50,
+    borderColor: colors.heavyBlue,
+    borderWidth: 2,
+    borderRadius: 5,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+    fontSize: 17,
+  },
+  button: {
+    backgroundColor: colors.heavyBlue,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 20,
+    fontWeight: "600",
+  },
+  registroText: {
+    marginTop: 30,
+    fontSize: 18,
+    color: colors.lightBlue,
+  },
+});
 
-const styles = StyleSheet.create({})
+export default Register;
